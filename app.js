5af93953d41dc574db6ba19d2fb636a10f7bbee2
vidhyakalanatarajan <143621235+vidhyakalanatarajan@users.noreply.github.com>
2023-10-27 10:13:15 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,16 @@
+function Box1(){
+    document.getElementById("color").className="color1";
+    document.getElementById("text").textContent="#e0e0e0";
+}
+function Box2(){
+    document.getElementById("color").className="color2";
+    document.getElementById("text").textContent="#6cfc97";
+}
+function Box3(){
+    document.getElementById("color").className="color3";
+    document.getElementById("text").textContent="#56ccf2";
+}
+function Box4(){
+    document.getElementById("color").className="color4";
+    document.getElementById("text").textContent="#bb6b97";
+}
